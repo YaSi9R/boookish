6166917e5b564dd0582fd1384956b3451cb232fb
Yasir <Yasir@gmail.com>
2024-08-25 23:43:06 +0530

    updating background image

--- react-app/src/Pages/Home.jsx
+++ react-app/src/Pages/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-
+import backgroundImage from "../assets/Images/website-background.png";
 const Home = () => {
   return (
-    <div>
-        
-      
-    </div>
+    <div className="relative overflow-hidden h-screen">
+    <img
+      src={backgroundImage}
+      alt="backgroundImage"
+      className="w-full h-full object-cover transition-transform duration-500 ease-in-out hover:scale-110"
+    />
+  </div>
+  
+
   )
 }
 
