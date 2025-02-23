1e6bb8b96181d879a17a7f8018a82a5a4eaf2861
mohd_yasir <mohdarman2369@gmail.com>
2025-02-23 22:20:37 +0530

    updated navbar functionality

--- react-app/src/data/navbar-links.js
+++ react-app/src/data/navbar-links.js
@@ -1,22 +1,30 @@
-export const NavbarLinks = [
-    {
-      title: "Home",
-      path: "/",
-    },
-    {
-      title: "Used Books On Sale",
-      path: '/usedBooks',
-    },
-    {
-      title: "Competitice Exam Boooks",
-      path: "/competiticebooks",
-    },
-    {
-      title: "Novels",
-      path: "/novels",
-    },
-    {
-        title:"Book Reviews",
-        path:'/bookreviews',
-    },
-  ];
+
+
+
+
+
+export const CompetitiveDropdown = [
+  { name: "CAT/GATE/GRE BOOKS", path: "/usedBooks/catgrebooks " },
+  { name: "ENGINEERING BOOKS", path: "/usedBooks/engineeringexambooks" },
+  { name: "NEET/AIIMS BOOKS", path: "/usedBooks/neetaiimsbooks" },
+  { name: "UPSC BOOKS", path: "/usedBooks/upscbooks" },
+
+
+];
+
+export const serviceDropdown = [
+  { name: "SCHOOL BOOKS", path: "/competitivebooks/schoolbooks" },
+  { name: "COLLEGE BOOKS", path: "/competitivebooks/collegebooks" },
+  { name: "MEDICAL", path: "/competitivebooks/medical" },
+  { name: "ENGINEERING", path: "/competitivebooks/engineering" },
+  { name: "OTHERS", path: "/competitivebooks/others" },
+
+
+
+];
+export const novelDropdown = [
+  { name: "Fiction", path: "/novelbooks/fiction" },
+  { name: "Non-Fiction", path: "/novelbooks/nonfiction" },
+
+
+]
