9030032d6629c2a2393f7db12d1374ad1d69c02b
mohd_yasir <mohdarman2369@gmail.com>
2025-02-24 23:54:54 +0530

    connecting server

--- node-app/index.js
+++ node-app/index.js
@@ -1,5 +1,5 @@
 const express=require("express");
-const app=express;
+const app=express();
 const database=require("./database");
 const cors = require("cors");
 const PORT = process.env.PORT || 4000;
@@ -9,27 +9,27 @@ const { cloudinaryConnect } = require("./config/cloudinary");
 const dotenv = require("dotenv");
 dotenv.config();
 
-database.connect();
+// database.connect();
 // Middlewares
-app.use(express.json());
-app.use(cookieParser());
-app.use(
-    cors({
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST", "PUT", "DELETE"],
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use(
+//     cors({
+//         origin: "http://localhost:3000",
+//         methods: ["GET", "POST", "PUT", "DELETE"],
 
-        credentials: true,
-    })
-);
+//         credentials: true,
+//     })
+// );
 
 cloudinaryConnect();
-app.get("/", (req, res) => {
-    return res.json(
-        {
-            success: true,
-            message: 'Your server is up and running....',
-        });
-});
+// app.get("/", (req, res) => {
+//     return res.json(
+//         {
+//             success: true,
+//             message: 'Your server is up and running....',
+//         });
+// });
 
 app.listen(PORT, () => {
     console.log(`App is running at ${PORT}`);
