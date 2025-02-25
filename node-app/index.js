70251e38d58378761f2cbfd336586f2d501e7c01
mohd_yasir <mohdarman2369@gmail.com>
2025-02-25 22:45:42 +0530

    updating server

--- node-app/index.js
+++ node-app/index.js
@@ -1,30 +1,17 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 require("dotenv").config();
+
 // Database Connecting
 const mongoose = require("mongoose");
 const database=require('./config/database');
 database.connect();
-/*
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/bookishDB", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-
-}).then(() => {
-    console.log("Database Connected SuccessFully");
-}
-
-).catch((error) => {
-    console.log("DB Connection Failed");
-    console.error(error);
-    process.exit(1);
-
-}) */
 
 
 
-app.use(express.json());
+
+
 app.listen(4000, () => {
     console.log("Server Connected successfully: ")
 })
@@ -39,4 +26,3 @@ app.post('/api/cars', (req, res) => {
     console.log(price);
 
 })
-console.log("MongoDB URL:", process.env.MONGODB_URL); // Check if it's defined
