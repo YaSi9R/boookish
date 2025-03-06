7e0c7b6f7b054fe9c0fb6aee81e7ba8bbf25d78e
mohd_yasir <mohdarman2369@gmail.com>
2025-03-06 12:00:44 +0530

    updating dropdown

--- react-app/src/Components/Core/SellButton.jsx
+++ react-app/src/Components/Core/SellButton.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const SellButton = () => {
+const SellButton = ({onClick}) => {
   return (
     
-      <Link to= "sellBooks">
-      <div className="w-[175px] text-sm h-[70px] bg-[#E74C3C] py-[14px] px-[35px] hover:bg-[#b52417] duration-[75] " >
+      <Link to= "sellBooks" onClick={onClick}>
+      <div className="w-[175px] text-sm h-[70px] bg-[#E74C3C] py-[14px] px-[35px] hover:bg-[#b52417] duration-[75] "  >
         <div className="text-center text-[white] py-[10px] font-semibold ">
           <i className="fa fa-plus pr-[2px]  "></i>
           SELL BOOKS
