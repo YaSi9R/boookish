b92e0d3d649d9fb4e88c0249a492064ad6e840bb
mohd_yasir <mohdarman2369@gmail.com>
2025-03-07 23:51:35 +0530

    updating backend logic

--- node-app/index.js
+++ node-app/index.js
@@ -8,12 +8,16 @@ const mongoose = require("mongoose");
 const database=require('./config/database');
 database.connect();
 
+// Agar mujhe parsing sambhaalni hai to me body-parser ka use karunga
+// specifially use karte hain ham post and updtae ke case me
+const bodyParser=require("body-parser");
+app.use(bodyParser.json());
 
 
 
 
 app.listen(4000, () => {
-    console.log("Server Connected successfully: ")
+    console.log("Server Connected successfully:  and is running at:4000 ")
 })
 
 app.get('/', (req, res) => {
