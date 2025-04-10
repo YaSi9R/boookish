e7b5eb8e04e189e30c6d69d87d0005bd904c4948
mohd_yasir <mohdarman2369@gmail.com>
2025-04-10 23:23:29 +0530

    updating user model

--- node-app/models/User.js
+++ node-app/models/User.js
@@ -1,12 +1,8 @@
 const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema(
     {
-        firstName: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-        lastName: {
+       
+        Name: {
             type:String,
             required:true,
             trim:true,
