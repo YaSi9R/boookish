10a43cac3ae470a9830fab2d508277a0cd46f286
mohd_yasir <mohdarman2369@gmail.com>
2025-05-13 23:47:30 +0530

    update login redux state

--- react-app/src/Pages/Login.jsx
+++ react-app/src/Pages/Login.jsx
@@ -8,7 +8,7 @@ import { FcGoogle } from "react-icons/fc";
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 import { login } from "../services/operations/authAPI";
 import { useDispatch } from "react-redux";
-import LoadingModal from "../Components/Common/LoadingModal"; // Import the LoadingModal
+import LoadingModal from "../Components/Common/LoadingModal"; 
 
 const Login = () => {
   const dispatch = useDispatch();
