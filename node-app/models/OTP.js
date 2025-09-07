d897e6cab2547c840c0822d768c573c3e0376f75
mohd_yasir <mohdarman2369@gmail.com>
2025-09-07 20:46:13 +0530

    change in otp model

--- node-app/models/OTP.js
+++ node-app/models/OTP.js
@@ -1,41 +1,38 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 const mailSender = require("../utils/mailSender");
-const emailTemplate=require("../mail/templates/emailVerificationTemplate")
-const OTPSchema=new mongoose.Schema({
-    email:{
-        type:String,
-        required:true,
-    },
-    otp:{
-        type:String,
-    },
-    createdAt:{
-        type:Date,
-        default:Date.now,
-        expires:60*5,
-    },
+const emailTemplate = require("../mail/templates/emailVerificationTemplate");
+
+const OTPSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  otp: { type: String, required: true },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    expires: 300, // 5 minutes
+    index: true
+  }
 });
 
-async function sendVerifcationEmail(email,otp) {
-    try{
-        const mailResponse=await mailSender(email,"Verification Email from Bookish",emailTemplate(otp));
-        console.log(mailResponse);
-        console.log("Email sent successfully: ", mailResponse.response);
-
-    }
-    catch(error) {
-        console.log("Error occured in sending otp in otpSchema");
-        throw error;
-    }
+async function sendVerificationEmail(email, otp) {
+  try {
+    const mailResponse = await mailSender(
+      email,
+      "Verification Email from Bookish",
+      emailTemplate(otp)
+    );
+    console.log("Email sent successfully:", mailResponse.response);
+  } catch (error) {
+    console.log("Error occurred in sending OTP email:", error);
+    throw error;
+  }
 }
 
-
-OTPSchema.pre("save",async function(next) {
-if (this.isNew) {
-		await sendVerifcationEmail(this.email, this.otp);
-	}   
-     next();
+OTPSchema.pre("save", async function (next) {
+  if (this.isNew) {
+    await sendVerificationEmail(this.email, this.otp);
+  }
+  next();
 });
 
-const OTP=mongoose.model("OTP",OTPSchema);
-module.exports=OTP;
+const OTP = mongoose.model("OTP", OTPSchema);
+module.exports = OTP;
